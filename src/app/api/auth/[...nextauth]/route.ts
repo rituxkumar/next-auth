636fae31abc6cc1d035a381034c16a5f5d6f723b
Ritu kumar <ritu748841@gmail.com>
2025-08-26 17:11:48 +0530

    api request

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,30 @@
+
+// import { log } from "console";
+// import { logDisallowedDynamicError } from "next/dist/server/app-render/dynamic-rendering";
+// import { NextRequest, NextResponse } from "next/server";
+
+// export function GET(req: NextRequest) {
+//   return NextResponse.json({
+//     email: "ritu@gmail.com",
+//     name: "ritu",
+//   });
+// }
+
+// export async function POST(req: NextRequest) {
+//   // body
+//   const body = await req.json();
+
+//   // header
+
+//   console.log(req.nextUrl.searchParams.get("name"));
+
+//   // query parameters
+
+//   console.log(req.nextUrl.searchParams.get("name"));
+
+//   return NextResponse.json({
+//     message: "you are signed up",
+//   });
+// }
+
+
